469ac990301e89d0eca4a52db20ed63163ec7e8d
WaritW <Kaofang25tt@gmail.com>
2024-04-24 09:49:38 +0700

    [Created]: สร้างแบบฝึกหัดข้อ 3-12

---
+++ lesson3/assignment3-12.py
@@ -0,0 +1,7 @@
+#มีตัวแปร text เก็บสตริงอยู่แล้ว ถ้าใน text มี Python หรือ python ปรากฏอยู่ให้แสดง Yes ไม่เช่นนั้นแสดง No
+
+#คำตอบ
+if "Python" in text or "python" in text:
+    print("Yes")
+else:
+    print("No")
