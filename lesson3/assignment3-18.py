17921a83467af47a077e20c97abd11238ab51e58
WaritW <Kaofang25tt@gmail.com>
2024-04-24 10:19:02 +0700

    [Created]: สร้างแบบฝึกหัดข้อ 3-18

---
+++ lesson3/assignment3-18.py
@@ -0,0 +1,14 @@
+#เขียนโปรแกรมตามผังงาน
+
+#คำตอบ
+if m == 'a':
+    t = "ant"
+elif m == 'b':
+    t = "bat"
+elif m == 'c':
+    t = "cat"
+elif m == 'd':
+    t = "dog"
+else:
+    t = "???"
+print(t)
